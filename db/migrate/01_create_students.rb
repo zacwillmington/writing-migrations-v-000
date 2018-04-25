226fbb96b54e6da41bfee239b76d348e532c4f19
Zac Willmington <zac.willmington@gmail.com>
2018-04-25 21:20:42 +0000

    automatically backed up by learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,5 +1,11 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
     def change
-        create_table(name: string:)
+        create_table(students) do |t|
+            t.column :name, :string
+         end
     end
+end
+create_table(:suppliers) do |t|
+  t.column :name, :string, limit: 60
+  # Other fields here
 end
