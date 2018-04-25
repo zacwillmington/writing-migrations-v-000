7c36e43a8c3d154d2dd990d199fcd965430895da
Zac Willmington <zac.willmington@gmail.com>
2018-04-25 22:19:07 +0000

    automatically backed up by learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,6 +1,6 @@
 class StringToDateTime < ActiveRecord::Migration[5.1]
     def change
-        change_column(:students, :birthdate, :datetime)
+        change_column :students, :birthdate, :datetime
          end
     end
 end
