a8e0a4963ec15e4b30df7af2b285ae7d080ef449
Zac Willmington <zac.willmington@gmail.com>
2018-04-25 22:04:07 +0000

    automatically backed up by learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,7 +1,7 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
     def change
         create_table(:students) do |t|
-            t.column :name, :string
+            t.string :name
          end
     end
 end
